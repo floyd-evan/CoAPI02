6d38b8f898c77bf545fd44433590e144959066c9
Floyd <floydevan.bestudio@outlook.com>
2020-10-26 17:42:12 +0800

    Added models to the project.

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -5,11 +5,12 @@ namespace CommandAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-
+    
     public class CommandsController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get(){
+        public ActionResult<IEnumerable<string>> Get()
+        {
             return new string [] {"This", "is", "Hardcoded"};
         }
     }
